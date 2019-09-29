8e8c1e6f40747d9d658a69b853ff44a82dbdeb6f
Alabi johnson <abiodunalabijohn@gmail.com>
2019-09-29 12:35:34 +0100

    choosesectionadded

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
 import './App.css';
 import AboutUs from './component/element/AboutUs/AboutUs';
-
+import Choose from './component/element/Choose/Choose';
+import Service from './component/element/Service/Service';
 function App() {
   return (
     <div className="App">
      <AboutUs />
+     <Choose />
+     <Service />
     </div>
   );
 }
---
+++ src/component/element/Service/Service.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Service.css';
+ class Service extends React.Component {
+    render() {
+        return (
+            <div>
+              <section className="section-service">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                        <div className="choose-heading">Our Services</div>
+                        </div>
+                    </div>
+                </div>
+              </section>
+            </div>
+        )
+    }
+}
+export default Service;
