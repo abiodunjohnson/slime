6a9f590f04eb57444012a7ad884fd0eb622896b0
abayomi igwubor <igwubor@outlook.com>
2019-10-02 16:27:50 +0100

    updatedSmile

--- src/component/element/Smile.jsx
+++ src/component/element/Smile.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Slider from './NavBar/Slider';
-import NavigationBar from './NavBar/Nav'
+import NavigationBar from './NavBar/Nav';
+import About from './AboutUs/About';
+import Choose from './Choose/Choose';
+import Service from './Service/Service';
+import Clients from './Clients/Clients';
+
 
 function Smile() {
 	return (
 		<div>
 			<NavigationBar/>
 			<Slider />
+			<About/>
+			<Choose/>
+			<Service/>
+			<Clients/>
 		
 		</div>
 	);
