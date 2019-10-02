cd427313711cfbca5f616cbfb038aedad95a6522
Alabi johnson <abiodunalabijohn@gmail.com>
2019-10-02 13:06:25 +0100

    clientsadded

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import AboutUs from './component/element/AboutUs/AboutUs';
 import Choose from './component/element/Choose/Choose';
 import Service from './component/element/Service/Service';
+import Clients from './component/element/Clients/Clients';
 function App() {
   return (
     <div className="App">
      <AboutUs />
      <Choose />
      <Service />
+     <Clients />
     </div>
   );
 }
