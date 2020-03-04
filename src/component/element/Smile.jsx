727daaffe2c05975341313d0955524a86a868569
abayomi igwubor <igwubor@outlook.com>
2020-03-04 01:36:12 +0100

    madeChangesToNavbar

--- src/component/element/Smile.jsx
+++ src/component/element/Smile.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Slider from './NavBar/Slider';
-import NavigationBar from './NavBar/Nav';
+// import NavigationBar from './NavBar/Nav';
 import About from './AboutUs/About';
 import Choose from './Choose/Choose';
 import Service from './Service/Service';
 import Clients from './Clients/Clients';
 import Footer from  './Footer/Footer';
+import NewNavBar from  './NewNavBar/NewNavBar';
 
 
 function Smile() {
 	return (
 		<div>
-			<NavigationBar/>
+		
+		<NewNavBar/>
+			
 			<Slider />
 			<About/>
 			<Choose/>
